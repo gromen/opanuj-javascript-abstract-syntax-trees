f74bc9dafa8ccfa03c966249e8b84552fa0b084f
Marcin Gromek <gromson101@gmail.com>
2023-04-13 10:57:52 +0200

    Use nested syntax tree

--- index.js
+++ index.js
@@ -56,8 +56,104 @@ verify(
   convertAstToHtmlString({
     nodeType: 'element',
     tagName: 'div',
-    attributes: [{ name: 'class', value: 'test' }],
-    children: [{ nodeType: 'text', value: 'Hello world!' }],
+    attributes: [
+      {
+        name: 'class',
+        value: 'profile',
+      },
+    ],
+    children: [
+      {
+        nodeType: 'element',
+        tagName: 'img',
+        attributes: [
+          {
+            name: 'class',
+            value: 'profile__avatar',
+          },
+          {
+            name: 'src',
+            value: 'https://www.thispersondoesnotexist.com/image',
+          },
+          {
+            name: 'alt',
+            value: 'Avatar',
+          },
+        ],
+      },
+      {
+        nodeType: 'element',
+        tagName: 'div',
+        attributes: [
+          {
+            name: 'class',
+            value: 'profile__details',
+          },
+        ],
+        children: [
+          {
+            nodeType: 'element',
+            tagName: 'p',
+            attributes: [
+              {
+                name: 'class',
+                value: 'profile__name',
+              },
+            ],
+            children: [
+              {
+                nodeType: 'text',
+                value: 'John Doe',
+              },
+            ],
+          },
+          {
+            nodeType: 'element',
+            tagName: 'p',
+            attributes: [
+              {
+                name: 'class',
+                value: 'profile__phone',
+              },
+            ],
+            children: [
+              {
+                nodeType: 'text',
+                value: '+48 123 456 789',
+              },
+            ],
+          },
+          {
+            nodeType: 'element',
+            tagName: 'p',
+            attributes: [
+              {
+                name: 'class',
+                value: 'profile__link',
+              },
+            ],
+            children: [
+              {
+                nodeType: 'element',
+                tagName: 'a',
+                attributes: [
+                  {
+                    name: 'href',
+                    value: 'https://przeprogramowani.pl/o-nas',
+                  },
+                ],
+                children: [
+                  {
+                    nodeType: 'text',
+                    value: 'Zobacz więcej',
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
   }),
   '<div class="test">Hello world!</div>'
 );
